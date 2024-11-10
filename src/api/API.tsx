73d26824b3170d04a44255d32f74f53edee54b9f
RebeccaZhu29 <pingbozhu59@gmail.com>
2024-11-10 15:32:45 +1100

    first comit

---
+++ src/api/API.tsx
@@ -0,0 +1,47 @@
+const searchGithub = async () => {
+  try {
+    const start = Math.floor(Math.random() * 100000000) + 1;
+    console.log('import.meta.env=', import.meta.env);
+    console.log('fjaifjeijgfojie')
+    const response = await fetch(
+      `https://api.github.com/users?since=${start}`,
+      {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }
+    );
+    console.log('Response:', response);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    console.log('Data:', data);
+    return data;
+  } catch (err) {
+    // console.log('an error occurred', err);
+    return [];
+  }
+};
+
+const searchGithubUser = async (username: string) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    return data;
+  } catch (err) {
+    // console.log('an error occurred', err);
+    return {};
+  }
+};
+
+export { searchGithub, searchGithubUser };
